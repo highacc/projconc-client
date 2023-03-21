dd193523f2bc0d27f49743bdd761d3fb79098e89
Olavo e Souto <olavo_souto@hotmail.com>
2023-03-21 09:08:16 -0300

    React Setup with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  const [clickCount, setClickCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +12,16 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Button onClick={() => setClickCount(clickCount+1)}>
+        Add Count
+       </Button>
+       <div>
+       Click Count = {clickCount}
+       </div>
+       <Button onClick={() => setClickCount(clickCount-1)}>
+        Reduce Count 
+       </Button>
+       
       </header>
     </div>
   );
